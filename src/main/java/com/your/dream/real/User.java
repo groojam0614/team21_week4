0ed411523827f1aeb8b842b4b70cd7bda67511f4
groojam0614 <ayfls0614@naver.com>
2016-04-01 00:36:49 +0900

    test

---
+++ src/main/java/com/your/dream/real/User.java
@@ -0,0 +1,12 @@
+package com.your.dream.real;
+
+public class User {
+	private String userName;
+	private String address;
+	private int lineNum;
+	private int min;
+	private Grade grade;
+	
+	
+	
+}
